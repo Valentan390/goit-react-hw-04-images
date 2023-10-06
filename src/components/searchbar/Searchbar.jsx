6ab7681e8cc1dd6f148c575781e0aa179202b4ed
Valentan390 <137200405+Valentan390@users.noreply.github.com>
2023-10-06 11:56:13 +0300

    Uodate

--- src/components/searchbar/Searchbar.jsx
+++ src/components/searchbar/Searchbar.jsx
@@ -10,7 +10,6 @@ export const Searchbar = ({ onSubmit }) => {
     event.preventDefault();
     onSubmit(inputData);
     setinputData('');
-    // this.setState({ inputData: '' });
   };
 
   const onChangeInput = event => {
@@ -42,47 +41,3 @@ export const Searchbar = ({ onSubmit }) => {
     </div>
   );
 };
-
-// export class Searchbar extends Component {
-//   state = {
-//     inputData: '',
-//   };
-
-//   handleSubmit = event => {
-//     event.preventDefault();
-//     this.props.onSubmit(this.state.inputData);
-//     this.setState({ inputData: '' });
-//   };
-
-//   onChangeInput = event => {
-//     const { value } = event.target;
-//     this.setState({ inputData: value });
-//   };
-
-//   render() {
-//     const { inputData } = this.state;
-//     return (
-//       <div>
-//         <header className={styles.Searchbar}>
-//           <form onSubmit={this.handleSubmit} className={styles.SearchForm}>
-//             <button type="submit" className={styles.SearchFormButton}>
-//               <ImSearch size={25} />
-//               {/* <span class="button-label">Search</span> */}
-//             </button>
-
-//             <input
-//               name="inputData"
-//               value={inputData}
-//               onChange={this.onChangeInput}
-//               className={styles.SearchFormInput}
-//               type="text"
-//               autocomplete="off"
-//               autofocus
-//               placeholder="Search images and photos"
-//             />
-//           </form>
-//         </header>
-//       </div>
-//     );
-//   }
-// }
